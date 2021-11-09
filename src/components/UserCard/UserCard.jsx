16bfc3621253ca76d26f9b1bf376111ea4224b70
Mikael Larsson <mikael.j.larsson@hotmail.com>
2021-11-09 10:56:58 +0100

    Broke out User Card into its own component

---
+++ src/components/UserCard/UserCard.jsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar as starFilled } from '@fortawesome/free-solid-svg-icons';
+import { faStar as starOutlined } from '@fortawesome/free-regular-svg-icons';
+
+import './user-card.css';
+
+const FavoriteIcon = props => (
+  <FontAwesomeIcon
+    icon={props.filled ? starFilled : starOutlined}
+    className="favorites-icon"
+  />
+);
+
+const UserCard = props => {
+  return (
+    <div className="user-card-content">
+      <p>
+        <Link to="/user"><strong>{ props.user.name }</strong></Link><br />
+        { props.user.company }<br />
+        <a href={ props.user.email }>{ props.user.email }</a>
+        <FavoriteIcon filled={props.user.isFavorite} />
+      </p>
+    </div>
+  );
+}
+
+export default UserCard;
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -2,15 +2,9 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
-import { faStar as starIconOutlined } from '@fortawesome/free-regular-svg-icons';
+import UserCard from './UserCard/UserCard';
 
 import "../App.css";
-
-const StarFilled = props => <FontAwesomeIcon icon={faStar} {...props} />
-const StarOutlined = () => <FontAwesomeIcon icon={starIconOutlined} />
 
 const Users = () => {
     return (
@@ -22,113 +16,27 @@ const Users = () => {
             <h1>Favorites</h1>
             <Row xs={2} md={3} lg={4} xl={5} className="userList">
                 <Col>
-                    <div className="user-card-content">
-                        <p>
-                            <Link to="/user"><strong>Tobias</strong></Link><br />
-                            Bontouch AB <br />
-                            <a href="tobias.arfors@bontouch.com">tobias.arfors@bontouch.com</a>
-                            <StarFilled className="favorites-icon" />
-                        </p>
-                    </div>
-                </Col>
-                <Col>
-                    <div className="user-card-content">
-                        <p>
-                            <Link to="/user"><strong>Tobias</strong></Link><br />
-                            Bontouch AB <br />
-                            <a href="tobias.arfors@bontouch.com">tobias.arfors@bontouch.com</a>
-                            <StarFilled className="favorites-icon" />
-                        </p>
-                    </div>
-                </Col>
-                <Col>
-                    <div className="user-card-content">
-                        <p>
-                            <Link to="/user"><strong>Tobias</strong></Link><br />
-                            Bontouch AB <br />
-                            <a href="tobias.arfors@bontouch.com">tobias.arfors@bontouch.com</a>
-                            <StarFilled className="favorites-icon" />
-                        </p>
-                    </div>
+                    <UserCard user={{ 
+                        name: 'Mikael Larsson',
+                        company: 'Bontouch AB',
+                        email: 'mikael.larsson@bontouch.com',
+                        isFavorite: true
+                    }} />
                 </Col>
             </Row>
+            
             <h1>Users</h1>
             <Row xs={2} md={3} lg={4} xl={5} className="userList">
                 <Col>
-                    <div className="user-card-content">
-                        <p><strong>Tobias</strong><br />
-                        Bontouch AB <br />
-                        <a href="tobias.arfors@bontouch.com">tobias.arfors@bontouch.com</a>
-                        <StarOutlined className="favorites-icon" />
-                        </p>
-                    </div>
-                </Col>
-                <Col>
-                    <div className="user-card-content">
-                        <p><strong>Tobias</strong><br />
-                        Bontouch AB <br />
-                        <a href="tobias.arfors@bontouch.com">tobias.arfors@bontouch.com</a>
-                        <StarOutlined className="favorites-icon" />
-                        </p>
-                    </div>
-                </Col>
-                <Col>
-                    <div className="user-card-content">
-                        <p><strong>Tobias</strong><br />
-                        Bontouch AB <br />
-                        <a href="tobias.arfors@bontouch.com">tobias.arfors@bontouch.com</a>
-                        <StarOutlined className="favorites-icon" />
-                        </p>
-                    </div>
-                </Col>
-                <Col>
-                    <div className="user-card-content">
-                        <p><strong>Tobias</strong><br />
-                        Bontouch AB <br />
-                        <a href="tobias.arfors@bontouch.com">tobias.arfors@bontouch.com</a>
-                        <StarOutlined className="favorites-icon" />
-                        </p>
-                    </div>
-                </Col>
-                <Col>
-                    <div className="user-card-content">
-                        <p><strong>Tobias</strong><br />
-                        Bontouch AB <br />
-                        <a href="tobias.arfors@bontouch.com">tobias.arfors@bontouch.com</a>
-                        <StarOutlined className="favorites-icon" />
-                        </p>
-                    </div>
-                </Col>
-                <Col>
-                    <div className="user-card-content">
-                        <p><strong>Tobias</strong><br />
-                        Bontouch AB <br />
-                        <a href="tobias.arfors@bontouch.com">tobias.arfors@bontouch.com</a>
-                        <StarOutlined className="favorites-icon" />
-                        </p>
-                    </div>
-                </Col>
-                <Col>
-                    <div className="user-card-content">
-                        <p><strong>Tobias</strong><br />
-                        Bontouch AB <br />
-                        <a href="tobias.arfors@bontouch.com">tobias.arfors@bontouch.com</a>
-                        <StarOutlined className="favorites-icon" />
-                        </p>
-                    </div>
-                </Col>
-                <Col>
-                    <div className="user-card-content">
-                        <p><strong>Tobias</strong><br />
-                        Bontouch AB <br />
-                        <a href="tobias.arfors@bontouch.com">tobias.arfors@bontouch.com</a>
-                        <StarOutlined className="favorites-icon" />
-                        </p>
-                    </div>
+                    <UserCard user={{ 
+                        name: 'Mikael Larsson',
+                        company: 'Bontouch AB',
+                        email: 'mikael.larsson@bontouch.com',
+                        isFavorite: false
+                    }} />
                 </Col>
             </Row>
         </Container>
-            
     );
 }
 
