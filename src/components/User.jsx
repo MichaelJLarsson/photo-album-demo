03f904095bf3f09de8571196f77a0e5846288fd4
Mikael Larsson <mikael.j.larsson@hotmail.com>
2021-11-15 12:25:48 +0100

    Fetch user data from API

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,21 +1,50 @@
+import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { users } from '../data/users';
 
 import "../App.css";
 
 const User = () => {
   let { userId } = useParams();
-  // eslint-disable-next-line
-  const user = users.find(item => item.id == userId);
+
+  const [user, setUser] = useState({});
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    const url = `https://jsonplaceholder.typicode.com/users/${userId}`;
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setUser(json);
+      } catch (error) {
+        console.warn('Error', error);
+      }
+    }
+    fetchData();
+  }, [userId]);
+  
+  useEffect(() => {
+    const url = `https://jsonplaceholder.typicode.com/users/${userId}/albums`;
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setAlbums(json);
+      } catch (error) {
+        console.warn('Error', error);
+      }
+    }
+    fetchData();
+  }, [userId]);
   
   return (
     <Container className="content">
       <Breadcrumb>
-        <Breadcrumb.Item linkAs={Link} linkProps={{ to: "/" }}>Users</Breadcrumb.Item>
+        <Breadcrumb.Item linkAs={ Link } linkProps={{ to: "/" }}>Users</Breadcrumb.Item>
         <Breadcrumb.Item active="true">{ user.name }</Breadcrumb.Item>
       </Breadcrumb>
 
@@ -24,9 +53,9 @@ const User = () => {
           <div className="user-info">
             <h2>{ user.name }</h2>
             <p>
-              { user.company }<br />
+              { user.company?.name }<br />
               { user.email } <br />
-              { user.address }
+              { user.address?.street } / { user.address?.city }
             </p>
           </div>
         </Col>
@@ -34,10 +63,10 @@ const User = () => {
 
       <h2>Albums</h2>
       <Row xs={2} md={3} lg={4} xl={5}>
-        { user.albums.map((album, i) => (
+        { albums.map((album, i) => (
           <Col key={i}>
             <div className="album-list-item">
-              <Link to={`${album.slug}`}>{ album.name }</Link>
+              <Link to={`${album.id}`}>{ album.title }</Link>
             </div>
           </Col>
         )) }
